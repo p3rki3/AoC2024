871192ec07396844aad803aef25ed534eecb79af
Simon <41059048+p3rki3@users.noreply.github.com>
2024-12-01 09:57:32 +0000

    Day 1 Solution refactored

--- Day1_solution.py
+++ Day1_solution.py
@@ -1,12 +1,7 @@
 import AoCFramework as AoC
 
 nums = [line.split() for line in AoC.Init("data/day1.txt")[0]]
-N1, N2 = sorted([int(num[0]) for num in nums]), sorted([int(num[1]) for num in nums])
-part_1 = part_2 = 0
-
-for i in range(len(N1)):
-    part_1 += abs(N1[i] - N2[i])
-    part_2 += N1[i] * N2.count(N1[i])
-
+N1, N2, lenN1 = sorted([int(num[0]) for num in nums]), sorted([int(num[1]) for num in nums]), len(nums)
 AoC.verify(1151792, 21790168)
-AoC.run(part_1, part_2)
+AoC.run(sum(abs(N1[i] - N2[i]) for i in range(lenN1)), sum(N1[i] * N2.count(N1[i]) for i in range(lenN1)))
+
