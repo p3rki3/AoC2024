280778bf97fc2e2165cba55f76cc6cf21392ff17
Simon <41059048+p3rki3@users.noreply.github.com>
2024-12-06 15:22:23 +0000

    Day 6 Solution amended
    Amended to use sets instead of lists

--- Day6_solution.py
+++ Day6_solution.py
@@ -4,7 +4,7 @@ def part_1(block=None):
     global Visited
     r, c = start
     count = 1
-    Visited2 = [(r, c, (-1, 0))]
+    Visited2 = {(r, c, (-1, 0))}
     Dirns, dirptr = ((-1, 0), (0, 1), (1, 0), (0, -1)), 0
     while True:
         dr, dc = Dirns[dirptr]
@@ -15,25 +15,24 @@ def part_1(block=None):
             dirptr += 1 if dirptr < 3 else -3
         else:
             if (newr, newc) not in Visited and block is None:
-                Visited.append((newr, newc))
+                Visited.add((newr, newc))
                 count += 1
             elif block is not None:
                 if (newr, newc, Dirns[dirptr]) in Visited2:
                     return 0
                 else:
-                    Visited2.append((newr, newc, Dirns[dirptr]))
+                    Visited2.add((newr, newc, Dirns[dirptr]))
             r, c = newr, newc
 
 def part_2():
     return sum(1 if part_1(pos) == 0 else 0 for pos in Visited)
 
 Lines, numlines, linelen = AoC.Init("data/day6.txt")
-Visited = []
 for r in range(numlines):
     for c in range(linelen):
         if Lines[r][c] == '^':
             start = (r, c)
-            Visited = [(r, c)]
+            Visited = {(r, c)}
 
 AoC.verify(4374, 1705)
 AoC.run(part_1, part_2)
