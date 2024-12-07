5f987f4fe7c6697d523c5993d5f6f02b51150faa
Simon <41059048+p3rki3@users.noreply.github.com>
2024-12-07 07:43:28 +0000

    Day 7 Solution amended
    Some refactoring using list comprehensions + simplifications

--- Day7_solution.py
+++ Day7_solution.py
@@ -4,8 +4,8 @@ from itertools import product
 def solve(opers):
     answer = 0
     for eq in Equations:
-        res, inp, opsneeded, interim = eq[0], eq[1], len(eq[1]) - 1, 0
-        operator_list = [list(opers) for _ in range(opsneeded)]
+        res, inp, opsneeded = eq[0], eq[1], len(eq[1]) - 1
+        operator_list = [opers] * opsneeded
         for comb in product(*operator_list):
             interim = inp[0]
             for i in range(opsneeded):
@@ -20,12 +20,6 @@ def solve(opers):
                 break
     return answer
 
-Eqs_working = [line.split(':') for line in AoC.Init("data/day7.txt")[0]]
-Equations = []
-for eq in Eqs_working:
-    res = int(eq[0])
-    inplist = [int(num) for num in eq[1].split()]
-    Equations.append((res, inplist))
-
+Equations = [(int(eq[0]), [int(num) for num in eq[1].split()]) for eq in [line.split(':') for line in AoC.Init("data/day7.txt")[0]]]
 AoC.verify(5512534574980, 328790210468594)
 AoC.run(solve(['+', '*']), solve(['+', '*', '|']))
