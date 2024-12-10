f948ecc29226ebb2b28aace1a41fd38d637b760d
Simon <41059048+p3rki3@users.noreply.github.com>
2024-12-10 09:09:35 +0000

    Day 10 Solution Refactored

--- Day10_solution.py
+++ Day10_solution.py
@@ -13,14 +13,14 @@ def walk_path(x, y, prev_height, end_of_trail):
 
 def solve():
     part1, part2 = [], []
-    for p in [ Point(x, y) for y, line in enumerate(Grid) for x, c in enumerate(line) if c == 0 ]:
+    for start_p in [ Point(x, y) for y, line in enumerate(Grid) for x, c in enumerate(line) if c == 0 ]:
         end_of_trail = []
-        walk_path(p.x, p.y, -1, end_of_trail)
+        walk_path(start_p.x, start_p.y, -1, end_of_trail)
         part1.append(len(set(end_of_trail)))
         part2.append(len(end_of_trail))
     return sum(part1), sum(part2)
 
-Lines, numlines, linelen = AoC.Init("data/day10.txt", test=False, nolines=False, isnumlist=False, printme=False, pad=0, padchar=' ')
+Lines, numlines, linelen = AoC.Init("data/day10.txt")
 Point, Grid = namedtuple("Point", ("x", "y")), [[int(ch) for ch in line] for line in Lines]
 AoC.verify(607, 1384)
 AoC.run(solve())
