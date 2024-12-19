ef58d74195edd7a9101a9a661f56d04a725c1e25
Simon <41059048+p3rki3@users.noreply.github.com>
2024-12-19 06:51:09 +0000

    Day 19 Solution

---
+++ Day19_solution.py
@@ -0,0 +1,25 @@
+import AoCFramework as AoC
+
+def match_pattern(cloth, pattern):
+    if pattern:
+        ret_val = 0
+        if pattern in SubPattern:
+            return SubPattern[pattern]
+        for cloth in Cloth:
+            ret_val += match_pattern(Cloth, pattern[len(cloth):]) if pattern.startswith(cloth) else 0
+        SubPattern[pattern] = ret_val
+        return ret_val
+    return 1
+
+def solve() -> tuple:
+    part1 = part2 = 0
+    for pattern in Patterns:
+        pattern_match_count = match_pattern(Cloth, pattern)
+        part2 += pattern_match_count
+        part1 += 1 if pattern_match_count > 0 else 0
+    return part1, part2
+
+Lines = AoC.Init("data/day19.txt", test=False)[0]
+Cloth, Patterns, SubPattern = [c.strip() for c in Lines[0].split(',')], Lines[2:], dict()
+AoC.verify(347, 919219286602165)
+AoC.run(solve())
