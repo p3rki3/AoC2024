043805c3d78a61db651f14fcd7c8c1a4d0c1dd1c
Simon <41059048+p3rki3@users.noreply.github.com>
2024-12-19 07:16:39 +0000

    Day 19 Solution

--- Day19_solution.py
+++ Day19_solution.py
@@ -1,12 +1,12 @@
 import AoCFramework as AoC
 
-def match_pattern(cloth, pattern):
+def match_pattern(pattern: str) -> int:
     if pattern:
         ret_val = 0
         if pattern in SubPattern:
             return SubPattern[pattern]
         for cloth in Cloth:
-            ret_val += match_pattern(Cloth, pattern[len(cloth):]) if pattern.startswith(cloth) else 0
+            ret_val += match_pattern(pattern[len(cloth):]) if pattern.startswith(cloth) else 0
         SubPattern[pattern] = ret_val
         return ret_val
     return 1
@@ -14,7 +14,7 @@ def match_pattern(cloth, pattern):
 def solve() -> tuple:
     part1 = part2 = 0
     for pattern in Patterns:
-        pattern_match_count = match_pattern(Cloth, pattern)
+        pattern_match_count = match_pattern(pattern)
         part2 += pattern_match_count
         part1 += 1 if pattern_match_count > 0 else 0
     return part1, part2
