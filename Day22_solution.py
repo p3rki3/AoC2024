5ca83959025cf5603090dbad57989cdcbfdbb9fd
Simon <41059048+p3rki3@users.noreply.github.com>
2024-12-23 16:05:25 +0000

    DAy 22 & 23 Solutions amended

--- Day22_solution.py
+++ Day22_solution.py
@@ -1,37 +1,34 @@
 import AoCFramework as AoC
 
 def next_secret_num(num: int) -> int:
-    num1 = ((num * 64) ^ num) % 16777216
-    num2 = ((num1 // 32) ^ num1) % 16777216
-    return ((num2 * 2048) ^ num2) % 16777216
+    num1 = ((num << 6) ^ num) % 16777216
+    num2 = ((num1 >> 5) ^ num1)
+    return ((num2 << 11) ^ num2) % 16777216
 
-def get_all_scores(Prices, Price_changes):
-    scores = dict()
-    for i in range(len(Price_changes)-3):
-        pattern = (Price_changes[i], Price_changes[i+1], Price_changes[i+2], Price_changes[i+3])
+def get_all_scores(Prices):
+    scores, Price_changes = dict(), [Prices[i]-Prices[i-1] for i in range(1, len(Prices))]
+    for i in range(3, len(Price_changes)):
+        pattern = (Price_changes[i-3], Price_changes[i-2], Price_changes[i-1], Price_changes[i])
         if pattern not in scores:
-            scores[pattern] = Prices[i+4]
+            scores[pattern] = Prices[i+1]
     return scores
 
 def part_1():
     global Scores
     p1answer = 0
     for num in Nums:
-        Prices, nxt_num = [], num
+        Prices, nxt_num = [num % 10], num
         for i in range(2000):
             nxt_num = next_secret_num(nxt_num)
             Prices.append(nxt_num % 10)
         p1answer += nxt_num
-        Price_changes = [Prices[i+1]-Prices[i] for i in range(len(Prices)-1)]
-        newscores = get_all_scores(Prices, Price_changes)
+        newscores = get_all_scores(Prices)
         for key, val in newscores.items():
             if key not in Scores:
                 Scores[key] = val
-            else:
-                Scores[key] += val
+            else: Scores[key] += val
     return p1answer
 
-Nums = [int(num) for num in AoC.Init("data/day22.txt")[0]]
-Scores = dict()
+Nums, Scores = [int(num) for num in AoC.Init("data/day22.txt")[0]], dict()
 AoC.verify(19822877190, 2277)
 AoC.run(part_1(), max(Scores.values()) )
--- Day23_solution.py
+++ Day23_solution.py
@@ -1,9 +1,6 @@
 import AoCFramework as AoC
 import networkx as nx
 
-Lines = AoC.Init("data/day23.txt")[0]
-Graph = nx.Graph(line.split("-") for line in Lines)
-cliques = list(nx.enumerate_all_cliques(Graph))
-
+cliques = list(nx.enumerate_all_cliques(nx.Graph(line.split('-') for line in AoC.Init('data/day23.txt')[0])))
 AoC.verify(1423, 'gt,ha,ir,jn,jq,kb,lr,lt,nl,oj,pp,qh,vy')
-AoC.run(sum(len(c) == 3 and any(x[0] == 't' for x in c) for c in cliques), ",".join(sorted(cliques[-1])))
+AoC.run(sum(len(clq) == 3 and any(node[0] == 't' for node in clq) for clq in cliques), ','.join(sorted(cliques[-1])))
